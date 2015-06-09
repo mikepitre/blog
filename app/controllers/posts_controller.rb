3aadb75e0ea56d7ddbfd71da7fc289c45dc61cd5
Mike Pitre <michaelepitre@gmail.com>
2015-06-09 17:58:10 -0500

    added back buttons to views and edit functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,30 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(post_params)
 
     @post.save
+    redirect_to @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
     redirect_to @post
   end
 
